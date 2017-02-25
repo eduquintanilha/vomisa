604ae799263db5a771b1ef76382dc5ad3b1243d3
Eduardo Quintanilha <quintanilhaedu@gmail.com>
2017-02-25 10:43:17 -0300

    server não starta

--- server.py
+++ server.py
@@ -53,5 +53,6 @@ def webhook():
     return("ok",200)
 
 if __name__ == '__main__':
-	#port = int(os.environ.get("PORT",5000))
-	app.run(debug=True)
+	port = int(os.environ.get("PORT",5000))
+	#app.run(debug=True)
+	app.run(host='0.0.0.0', port=port)
