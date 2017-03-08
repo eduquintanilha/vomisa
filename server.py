88c4f2a3583eeadf7a533ffca51627a0ecf34d6c
Eduardo Quintanilha <quintanilhaedu@gmail.com>
2017-03-08 20:03:49 -0300

    Autenticando Webhook7

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ def handle_verification():
 if __name__ == '__main__':
     app.run(debug=True)
 # -----------------------------------------------------
-
+'''
 @app.route('/', methods=['POST'])
 
 def webhook():
